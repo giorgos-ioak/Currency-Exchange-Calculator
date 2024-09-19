576d823be13420a8559469001af91b61e1df4e72
Giorgos <giorgosmintis@gmail.com>
2024-09-19 18:55:29 +0300

    updated the endpoints

--- server/app.js
+++ server/app.js
@@ -10,8 +10,8 @@ app.use(express.json());
 
 
 function readFile() {
-  const fileContent = fs.readFileSync("./database/currencies.json" , "utf-8");          // METHOD FOR READING THE 'currencies.json' Database FILE.
-  const currencies = JSON.parse(fileContent);
+  const fileContent = fs.readFileSync("./database/currencies.json" , "utf-8");          // METHOD FOR READING THE 
+  const currencies = JSON.parse(fileContent);                                           // 'currencies.json' Database FILE.
 
   return currencies;
 };
@@ -26,87 +26,101 @@ function writeFile(data) {
 
 
 
-// app.get("/currencies" , (req,res) => {             // APP.GET
-//   const data = readFile();
-//   res.send(data);
-// });
+app.get("/currencies" , (req,res) => {
+  const currencies = readFile();
+
+  if(!currencies) {
+    return res.json({message: "There are no currencies registered"});
+  }
+
+  return res.send(currencies);
+});
 
 
 
-// app.get("/currencies/:name" , (req,res) => {      // APP.GET
-//   const name = req.params.name;
-//   const data = readFile();
+app.get("/currencies/:name" , (req,res) => {     
+  const currencies = readFile();
+  const name = req.params.name;
 
-//   const currency = data[name];
-//   return res.send(currency);
-// });
+  if(!currencies[name]) {
+    return res.json({message: "This currency does not exist"});
+  }
 
-
-
-// app.get("/convert" , (req,res) => {            // APP.POST
-//   const currencies = readFile();
-
-//   const baseCurrency = "USD";                  // FIXED VALUES
-//   const targetCurrency = "EUR";                 // WILL CHANGE LATER
-//   const amount = 1;
-
-//   const baseRate = currencies[baseCurrency].rate;
-//   const targetRate = currencies[targetCurrency].rate;
-//   const convertedAmount = (amount / baseRate) * targetRate;
-
-//   res.status(200).json({convertedAmount});
-// }); 
+  const currency = data[name];
+  return res.send(currency);
+});
 
 
 
+app.get("/convert" , (req,res) => {            
+  const currencies = readFile();
 
-// app.get("/createCurrency" , (req,res) => {                // APP.POST
-//   const currencies = readFile();    
-//   const name = "PPP";               // FIXED VALUES 
-//   const rate = 1.3552;              // WILL CHANGE LATER
+  const { baseCurrency } = req.body;
+  const { targetCurrency } = req.body;
+  const { amount } = req.body;
 
-//   if(currencies[name]) {
-//     return res.json({message: "This currency already exists."})
-//   };
 
-//   currencies[name] = { name, rate };
-//   writeFile(currencies);
+  const baseRate = currencies[baseCurrency].rate;
+  const targetRate = currencies[targetCurrency].rate;
+  const convertedAmount = (amount / baseRate) * targetRate;
 
-//   res.status(200).json({ message: "Currency Created" , currency: currencies[name]});
-// });
+  return res.status(200).json({convertedAmount});
+}); 
 
 
 
 
-// app.get("/currencies/:name" , (req,res) => {             // APP.PUT
-//   const currencies = readFile();
+app.post("/createCurrency" , (req,res) => {
+  const currencies = readFile();
+
+  const { name } = req.body;              
+  const { rate } = req.body;             
+
+  if(currencies[name]) {
+    return res.json({message: "This currency already exists."})
+  };
+
+  currencies[name] = { name, rate };
+  writeFile(currencies);
+
+  return res.status(200).json({ message: "Currency Created" , currency: currencies[name]});
+});
+
+
+
+
+app.put("/currencies/:name" , (req,res) => {
+  const currencies = readFile();
+
+  const name = req.params.name;
+  const { rate } = req.body;
+
+  if(!currencies[name]) {
+    return res.json({message: "This currency does not exist."})
+  };
+
+  currencies[name].rate = rate;
+
+  writeFile(currencies);
+  return res.status(200).json({message: "Updated the currency's rate" , currency: currencies[name]});
+});
+
+
+
+
+app.delete("/currencies/:name" , (req,res) => {
+  const currencies = readFile();
+  const name = req.params.name;
   
-//   const name = req.params.name;
-//   const rate = 1.5345;               // FIXED VALUE 
-//                                      // WILL CHANGE LATER
+  if(!currencies[name]) {
+    return res.json({message: "This currency does not exist."})
+  }
 
-//   currencies[name].rate = rate;
+  delete currencies[name];
 
-//   writeFile(currencies);
-//   res.status(200).json({message: "Updated the currency's rate" , currency: currencies[name]});
-// });
-
-
-
-
-// app.get("/currencies/:name" , (req,res) => {              // APP.DELETE
-//   const currencies = readFile();
-//   const name = req.params.name;
-  
-//   if(!currencies[name]) {
-//     return res.json({message: "This currency does not exist."})
-//   }
-
-//   delete currencies[name];
-
-//   writeFile(currencies);
-//   res.status(200).json({message: `Successfully deleted the currency.`})
-// });
+  writeFile(currencies);
+  return res.status(200).json({message: `Successfully deleted the currency.`})
+});
 
 
 
